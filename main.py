3f09b06c5606465eb85dc83b5213707b7be0c71b
Roy <88516395+moraroy@users.noreply.github.com>
2023-06-27 06:50:37 +0000

    awdadadadawdadadadwwda

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ class Plugin:
             os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
 
     async def install(self, selected_options):
+        decky_plugin.logger.info('main was called')
         # Set up logging
         logging.basicConfig(level=logging.DEBUG)
 
