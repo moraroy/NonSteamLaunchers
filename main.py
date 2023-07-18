3a0633d62b0b1dca9a360f2813ba07e7bfa5eb4d
Roy <88516395+moraroy@users.noreply.github.com>
2023-07-18 07:31:09 +0000

    adasd

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import subprocess
 import re
+import decky_plugin
 
 
 # Set up logging
@@ -16,7 +17,40 @@ def camel_to_title(s):
     return ' '.join(word.capitalize() for word in words)
 
 class Plugin:
-    def install(self, selected_options):
+     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
+    async def add(self, left, right):
+        return left + right
+
+    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+    async def _main(self):
+        decky_plugin.logger.info("Hello World!")
+
+    # Function called first during the unload process, utilize this to handle your plugin being removed
+    async def _unload(self):
+        decky_plugin.logger.info("Goodbye World!")
+        pass
+
+    # Migrations that should be performed before entering `_main()`.
+    async def _migration(self):
+        decky_plugin.logger.info("Migrating")
+        # Here's a migration example for logs:
+        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
+        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
+                                               ".config", "decky-template", "template.log"))
+        # Here's a migration example for settings:
+        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
+        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
+        decky_plugin.migrate_settings(
+            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
+            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
+        # Here's a migration example for runtime data:
+        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
+        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
+        decky_plugin.migrate_runtime(
+            os.path.join(decky_plugin.DECKY_HOME, "template"),
+            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        
+    async def install(self, selected_options):
         decky_plugin.logger.info('install was called')
         # Set up logging
         logging.basicConfig(level=logging.DEBUG)
