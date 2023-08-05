bef96a3f252e0bd571092dd8f0b4705a8a8982d1
Roy <88516395+moraroy@users.noreply.github.com>
2023-08-05 05:08:02 +0000

    asdasdasdasdasdasd

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -578,7 +578,18 @@ if [ ${#args[@]} -eq 0 ]; then
             echo "The cancel button was clicked"
             exit 1
         fi
+        # Check if the cancel button was clicked
+        if [ $? -eq 1 ] && [[ $options != "Start Fresh" ]] && [[ $options != "Move to SD Card" ]] && [[ $options != "Uninstall" ]] && [[ $options != "Find Games" ]]; then
+            # The cancel button was clicked
+            echo "The cancel button was clicked"
+            exit 1
+        fi
 
+        # Check if no options were selected and no custom website was provided
+        if [ -z "$options" ] && [ -z "$custom_websites" ]; then
+            # No options were selected and no custom website was provided
+            zenity --error --text="No options were selected and no custom website was provided. The script will now exit." --width=200 --height=150
+            exit 1
 
         # Split the custom_websites_str variable into an array using ',' as the delimiter
         IFS=',' read -ra custom_websites <<< "$custom_websites_str"
