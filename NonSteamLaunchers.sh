bfa0b665e8ac2e5676facc929e6bf82d3a543656
Roy <88516395+moraroy@users.noreply.github.com>
2023-08-05 04:32:21 +0000

    Revert if needed

--- NonSteamLaunchers.sh
+++ NonSteamLaunchers.sh
@@ -550,16 +550,9 @@ if [ ${#args[@]} -eq 0 ]; then
     # No command line arguments were provided, so display the main zenity window
     selected_launchers=$(zenity --list --text="Which launchers do you want to download and install?" --checklist --column="$version" --column="Default = one App ID Installation, One Prefix, NonSteamLaunchers" FALSE "SEPARATE APP IDS - CHECK THIS TO SEPARATE YOUR PREFIX'S" $epic_games_value "$epic_games_text" $gog_galaxy_value "$gog_galaxy_text" $uplay_value "$uplay_text" $origin_value "$origin_text" $battlenet_value "$battlenet_text" $amazongames_value "$amazongames_text" $eaapp_value "$eaapp_text" $legacygames_value "$legacygames_text" $itchio_value "$itchio_text" $humblegames_value "$humblegames_text" $indiegala_value "$indiegala_text" $rockstar_value "$rockstar_text" $glyph_value "$glyph_text" $minecraft_value "$minecraft_text" $psplus_value "$psplus_text" $dmm_value "$dmm_text" FALSE "Xbox Game Pass" FALSE "GeForce Now" FALSE "Amazon Luna" FALSE "Netflix" FALSE "Hulu" FALSE "Disney+" FALSE "Amazon Prime Video" FALSE "Youtube" --width=535 --height=740 --extra-button="Uninstall" --extra-button="Find Games" --extra-button="Start Fresh" --extra-button="Move to SD Card")
 
-     # Check if the user clicked either the 'Cancel' button or close button on the zenity window
-    if [ $? -eq 1 ]; then
-        # The user clicked either the 'Cancel' button or close button on the zenity window, so exit with exit code 1 to indicate failure.
-        echo "The cancel button was clicked"
-        exit 1
-    fi
-
-    # Check if the user selected one of the extra buttons
-    if [[ $selected_launchers == "Start Fresh" ]] || [[ $selected_launchers == "Move to SD Card" ]] || [[ $selected_launchers == "Uninstall" ]] || [[ $selected_launchers == "Find Games" ]]; then
-        # The user selected one of the extra buttons, so skip prompting for custom websites
+    # Check if the user clicked the 'Cancel' button or selected one of the extra buttons
+    if [ $? -eq 1 ] || [[ $selected_launchers == "Start Fresh" ]] || [[ $selected_launchers == "Move to SD Card" ]] || [[ $selected_launchers == "Uninstall" ]] || [[ $selected_launchers == "Find Games" ]]; then
+        # The user clicked the 'Cancel' button or selected one of the extra buttons, so skip prompting for custom websites
         custom_websites=()
     else
         # The user did not click the 'Cancel' button or select one of the extra buttons, so prompt for custom websites
