4430636cf47fdf98c03f25f7a161ce234aa60d90
Roy <88516395+moraroy@users.noreply.github.com>
2024-05-05 10:36:38 -0700

    fixed gog scanner bugs

--- py_modules/lib/scanners/gog_scanner.py
+++ py_modules/lib/scanners/gog_scanner.py
@@ -5,7 +5,7 @@ def getGogGameInfo(filePath):
     # Check if the file contains any GOG entries
     with open(filePath, 'r') as file:
         if "GOG.com" not in file.read():
-            decky_plugin.logger.info("No GOG entries found in the registry file. Skipping GOG Galaxy Games Scanner.")
+            print("No GOG entries found in the registry file. Skipping GOG Galaxy Games Scanner.")
             return {}
 
     # If GOG entries exist, parse the registry file
@@ -14,6 +14,8 @@ def getGogGameInfo(filePath):
         game_id = None
         game_name = None
         exe_path = None
+        depends_on = None
+        launch_command = None
         for line in file:
             split_line = line.split("=")
             if len(split_line) > 1:
@@ -24,16 +26,27 @@ def getGogGameInfo(filePath):
                 if "gameName" in line:
                     game_name = re.findall(r'\"(.+?)\"', split_line[1])
                     if game_name:
-                        game_name = game_name[0]
+                        game_name = bytes(game_name[0], 'utf-8').decode('unicode_escape')
+                        game_name = game_name.replace('!22', '™')
                 if "exe" in line and "GOG Galaxy" in line and not "unins000.exe" in line:
                     exe_path = re.findall(r'\"(.+?)\"', split_line[1])
                     if exe_path:
                         exe_path = exe_path[0].replace('\\\\', '\\')
-            if game_id and game_name and exe_path:
+                if "dependsOn" in line:
+                    depends_on = re.findall(r'\"(.+?)\"', split_line[1])
+                    if depends_on:
+                        depends_on = depends_on[0]
+                if "launchCommand" in line:
+                    launch_command = re.findall(r'\"(.+?)\"', split_line[1])
+                    if launch_command:
+                        launch_command = launch_command[0]
+            if game_id and game_name and launch_command:
                 game_dict[game_name] = {'id': game_id, 'exe': exe_path}
                 game_id = None
                 game_name = None
                 exe_path = None
+                depends_on = None
+                launch_command = None
 
     return game_dict
 
