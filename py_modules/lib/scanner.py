561e0d583cc06ccc9fbab606d32d8e3690997a64
Ben Mooney <ben@benmooney.co.uk>
2024-05-07 08:51:27 +0100

    Fix for no shortcuts.vdf file.

--- py_modules/lib/scanner.py
+++ py_modules/lib/scanner.py
@@ -118,12 +118,14 @@ def addCustomSite(customSiteJSON):
 
 def check_if_shortcut_exists(display_name, exe_path, start_dir, launch_options):
     # Load the existing shortcuts
-    with open(f"{logged_in_home}/.steam/root/userdata/{steamid3}/config/shortcuts.vdf", 'rb') as file:
-        shortcuts = vdf.binary_loads(file.read())
-    if any((s.get('appname') == display_name or s.get('AppName') == display_name) and (s.get('exe') == exe_path or s.get('Exe') == exe_path) and s.get('StartDir') == start_dir and s.get('LaunchOptions') == launch_options for s in shortcuts['shortcuts'].values()):
-        decky_plugin.logger.info(f"Existing shortcut found based on matching fields for game {display_name}. Skipping creation.")
-        return True
-    shortcuts = ''
+    vdf_path = f"{logged_in_home}/.steam/root/userdata/{steamid3}/config/shortcuts.vdf"
+    if os.path.exists(vdf_path):
+        with open(vdf_path, 'rb') as file:
+            shortcuts = vdf.binary_loads(file.read())
+        if any((s.get('appname') == display_name or s.get('AppName') == display_name) and (s.get('exe') == exe_path or s.get('Exe') == exe_path) and s.get('StartDir') == start_dir and s.get('LaunchOptions') == launch_options for s in shortcuts['shortcuts'].values()):
+            decky_plugin.logger.info(f"Existing shortcut found based on matching fields for game {display_name}. Skipping creation.")
+            return True
+        shortcuts = ''
 
 # Add or update the proton compatibility settings
 def add_compat_tool(launchoptions):
