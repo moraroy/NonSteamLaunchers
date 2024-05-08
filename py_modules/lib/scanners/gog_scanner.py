cc07c3c63d190d25530a35b1cd2fda5965ef79af
Ben Mooney <ben@benmooney.co.uk>
2024-05-08 16:46:20 +0100

    fix GoG scanner bugs due to case sensitivity. Also remove directory check to allow for SD card games to be found.

--- py_modules/lib/scanners/gog_scanner.py
+++ py_modules/lib/scanners/gog_scanner.py
@@ -3,9 +3,10 @@ import os, re, decky_plugin
 #Gog Galaxy Scanner
 def getGogGameInfo(filePath):
     # Check if the file contains any GOG entries
+
     with open(filePath, 'r') as file:
         if "GOG.com" not in file.read():
-            print("No GOG entries found in the registry file. Skipping GOG Galaxy Games Scanner.")
+            decky_plugin.logger.info("No GOG entries found in the registry file. Skipping GOG Galaxy Games Scanner.")
             return {}
 
     # If GOG entries exist, parse the registry file
@@ -19,59 +20,46 @@ def getGogGameInfo(filePath):
         start_menu_link = None
         gog_entry = False
         for line in file:
-            if "GOG.com" in line:
-                gog_entry = True
-            if gog_entry:
-                split_line = line.split("=")
-                if len(split_line) > 1:
-                    if "gameID" in line:
-                        game_id = re.findall(r'\"(.+?)\"', split_line[1])
-                        if game_id:
-                            game_id = game_id[0]
-                    if "gameName" in line:
-                        game_name = re.findall(r'\"(.+?)\"', split_line[1])
-                        if game_name:
-                            game_name = bytes(game_name[0], 'utf-8').decode('unicode_escape')
-                            game_name = game_name.replace('!22', '™')
-                    if "exe" in line and not "unins000.exe" in line:
-                        exe_path = re.findall(r'\"(.+?)\"', split_line[1])
-                        if exe_path:
-                            exe_path = exe_path[0].replace('\\\\', '\\')
-                    if "dependsOn" in line:
-                        depends_on = re.findall(r'\"(.+?)\"', split_line[1])
-                        if depends_on:
-                            depends_on = depends_on[0]
-                    if "launchCommand" in line:
-                        launch_command = re.findall(r'\"(.+?)\"\s*$', split_line[1])
-                        if launch_command:
-                            # Remove leading and trailing whitespace from the path
-                            path = launch_command[0].strip()
-                            # Reconstruct the launch command with the cleaned path
-                            launch_command = f"\"{path}\""
-                    if "startMenuLink" in line:
-                        start_menu_link = re.findall(r'\"(.+?)\"', split_line[1])
-                        if start_menu_link:
-                            start_menu_link = start_menu_link[0]
-                if game_id and game_name and launch_command and start_menu_link and 'GOG.com' in start_menu_link:
-                    game_dict[game_name] = {'id': game_id, 'exe': exe_path}
-                    game_id = None
-                    game_name = None
-                    exe_path = None
-                    depends_on = None
-                    launch_command = None
-                    start_menu_link = None
+            split_line = line.split("=")
+            if len(split_line) > 1:
+                if "gameid" in line.lower():
+                    game_id = re.findall(r'\"(.+?)\"', split_line[1])
+                    if game_id:
+                        game_id = game_id[0]
+                if "gamename" in line.lower():
+                    game_name = re.findall(r'\"(.+?)\"', split_line[1])
+                    if game_name:
+                        game_name = bytes(game_name[0], 'utf-8').decode('unicode_escape')
+                        game_name = game_name.replace('!22', '™')
+                if "exe" in line.lower() and not "unins000.exe" in line.lower():
+                    exe_path = re.findall(r'\"(.+?)\"', split_line[1])
+                    if exe_path:
+                        exe_path = exe_path[0].replace('\\\\', '\\')
+                if "dependson" in line.lower():
+                    depends_on = re.findall(r'\"(.+?)\"', split_line[1])
+                    if depends_on:
+                        depends_on = depends_on[0]
+                if "launchcommand" in line.lower():
+                    launch_command = re.findall(r'\"(.+?)\"', split_line[1])
+                    if launch_command:
+                        launch_command = launch_command[0]
+            if game_id and game_name and launch_command:
+                game_dict[game_name] = {'id': game_id, 'exe': exe_path}
+                game_id = None
+                game_name = None
+                exe_path = None
+                depends_on = None
+                launch_command = None
 
     return game_dict
 
 def gog_scanner(logged_in_home, gog_galaxy_launcher, create_new_entry):
     # Define your paths
-    gog_games_directory = f"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{gog_galaxy_launcher}/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games"
     registry_file_path = f"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{gog_galaxy_launcher}/pfx/system.reg"
 
     # Check if the paths exist
-    if not os.path.exists(gog_games_directory) or not os.path.exists(registry_file_path):
-        decky_plugin.logger.info("One or more paths do not exist.")
-        decky_plugin.logger.info("GOG Galaxy game data not found. Skipping scanning for GOG Galaxy games.")
+    if not os.path.exists(registry_file_path):
+        decky_plugin.logger.info("Registry path does not exist, skipping GOG scanner.")
     else:
         game_dict = getGogGameInfo(registry_file_path)
 
