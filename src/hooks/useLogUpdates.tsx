bd7a52d9cb4d72f374e4b9ecd562cf40f8c947ae
Roy <88516395+moraroy@users.noreply.github.com>
2024-08-11 03:47:46 +0000

    test 14

--- src/hooks/useLogUpdates.tsx
+++ src/hooks/useLogUpdates.tsx
@@ -1,33 +1,180 @@
-import { useEffect, useState, useRef } from 'react';
+import {
+  DialogHeader,
+  ToggleField,
+  DialogBodyText,
+  DialogBody,
+  DialogButton,
+  Focusable,
+  ServerAPI,
+  ModalRoot,
+  SteamSpinner,
+  ProgressBarWithInfo
+} from "decky-frontend-lib";
+import { useState, VFC } from "react";
+import { notify } from "../../hooks/notify";
+import { useSettings } from "../../hooks/useSettings";
+import { scan, autoscan } from "../../hooks/scan";
+import { useLogUpdates } from "../../hooks/useLogUpdates"; // Import the hook
 
-export const useLogUpdates = (): string => {
-  const [log, setLog] = useState<string>('');
-  const logWsRef = useRef<WebSocket | null>(null);
+type LauncherInstallModalProps = {
+  closeModal?: () => void,
+  launcherOptions: {
+      name: string;
+      label: string;
+      URL: string;
+      streaming: boolean;
+      enabled: boolean;
+  }[],
+  serverAPI: ServerAPI
+};
 
-  useEffect(() => {
-    if (!logWsRef.current) {
-      logWsRef.current = new WebSocket('ws://localhost:8675/logUpdates');
+/**
+* The modal for selecting launchers.
+*/
+export const LauncherInstallModal: VFC<LauncherInstallModalProps> = ({ closeModal, launcherOptions, serverAPI }) => {
 
-      logWsRef.current.onmessage = (e) => {
-        setLog((prevLog) => `${prevLog}\n${e.data}`);
-      };
+  const [progress, setProgress] = useState({ percent: 0, status: '', description: '' });
+  const { settings, setAutoScan } = useSettings(serverAPI);
+  const [options, setOptions] = useState(launcherOptions);
+  const [separateAppIds, setSeparateAppIds] = useState(false);
+  const [operation, setOperation] = useState("");
+  const [showLog, setShowLog] = useState(false); // State to control log display
+  const log = useLogUpdates(); // Use the hook
 
-      logWsRef.current.onerror = (e) => {
-        console.error(`WebSocket error: ${e}`);
-      };
+  const handleToggle = (changeName: string, changeValue: boolean) => {
+      const newOptions = options.map(option => {
+          if (option.name === changeName) {
+              return {
+                  ...option,
+                  enabled: changeValue,
+              };
+          } else {
+              return option;
+          }
+      });
+      setOptions(newOptions);
+  };
 
-      logWsRef.current.onclose = (e) => {
-        console.log(`WebSocket closed: ${e.code} - ${e.reason}`);
-      };
-    }
+  const handleSeparateAppIdsToggle = (value: boolean) => {
+      setSeparateAppIds(value);
+  };
 
-    return () => {
-      if (logWsRef.current) {
-        logWsRef.current.close();
-        logWsRef.current = null;
+  const handleInstallClick = async (operation: string) => {
+      setOperation(operation);
+      setShowLog(true); // Show log updates after button click
+      console.log('handleInstallClick called');
+      const selectedLaunchers = options
+          .filter(option => option.enabled && !option.streaming);
+      console.log(`Selected options: ${selectedLaunchers.join(', ')}`);
+      let i = 0;
+      let previousAutoScan = settings.autoscan;
+      for (const launcher of selectedLaunchers) {
+          if (!launcher.streaming) {
+              setAutoScan(false);
+              console.log(`Calling ${operation} launcher method for ${launcher}`);
+              const launcherParam: string = (launcher.name.charAt(0).toUpperCase() + launcher.name.slice(1));
+              await installLauncher(launcherParam, launcher.label, i, operation);
+          }
+          i++;
       }
-    };
-  }, []);
+      scan();
+      setAutoScan(previousAutoScan);
+      if (settings.autoscan) { autoscan(); }
+  };
 
-  return log;
+  const installLauncher = async (launcher: string, launcherLabel: string, index: number, operation: string) => {
+      const total = options.filter(option => option.enabled).length;
+      const startPercent = index === 0 ? 0 : index / total * 100;
+      const endPercent = (index + 1) / total * 100;
+      console.log(`${operation} Launcher: ${launcherLabel}, Index: ${index}, StartPercent: ${startPercent}, EndPercent: ${endPercent}`);
+      setProgress({
+          percent: startPercent,
+          status: `${operation}ing Launcher ${index + 1} of ${total}`,
+          description: `${launcherLabel}`
+      });
+      try {
+          const result = await serverAPI.callPluginMethod("install", {
+              selected_options: launcher,
+              operation: operation,
+              install_chrome: false,
+              separate_app_ids: separateAppIds,
+              start_fresh: false // Pass true for the start_fresh parameter
+          });
+
+          if (result) {
+              setProgress({ percent: endPercent, status: `${operation} Selection ${index + 1} of ${total}`, description: `${launcher}` });
+              notify.toast(`Launcher ${operation}ed`, `${launcherLabel} was ${operation.toLowerCase()}ed successfully!`);
+          } else {
+              setProgress({ percent: endPercent, status: `${operation} selection ${index + 1} of ${total} failed`, description: `${operation} ${launcher} failed. See logs.` });
+              notify.toast(`${operation} Failed`, `${launcherLabel} was not ${operation.toLowerCase()}ed.`);
+          }
+      } catch (error) {
+          setProgress({ percent: endPercent, status: `Installing selection ${index + 1} of ${total} failed`, description: `Installing ${launcher} failed. See logs.` });
+          notify.toast("Install Failed", `${launcherLabel} was not installed.`);
+          console.error('Error calling _main method on server-side plugin:', error);
+      }
+  };
+
+  return ((progress.status != '' && progress.percent < 100) ?
+      <ModalRoot>
+          <DialogHeader>
+              {`${operation}ing Game Launchers`}
+          </DialogHeader>
+          <DialogBodyText>Selected options: {options.filter(option => option.enabled).map(option => option.label).join(', ')}</DialogBodyText>
+          <DialogBody>
+              <SteamSpinner />
+              <ProgressBarWithInfo
+                  layout="inline"
+                  bottomSeparator="none"
+                  sOperationText={progress.status}
+                  description={progress.description}
+                  nProgress={progress.percent}
+              />
+              {showLog && (
+                  <div style={{ fontSize: 'small', marginTop: '10px', whiteSpace: 'pre-wrap' }}>
+                      {log}
+                  </div>
+              )} {/* Render log updates */}
+          </DialogBody>
+      </ModalRoot> :
+      <ModalRoot onCancel={closeModal}>
+          <DialogHeader>
+              Select Game Launchers
+          </DialogHeader>
+          <DialogBodyText>Here you choose your launchers you want to install and let NSL do the rest. Once installed, they will be added your library!</DialogBodyText>
+          <DialogBody>
+              {launcherOptions.map(({ name, label }) => (
+                  <ToggleField
+                      label={label}
+                      checked={options.find(option => option.name === name)?.enabled ? true : false}
+                      onChange={(value) => handleToggle(name, value)}
+                  />
+              ))}
+          </DialogBody>
+          <p style={{ fontSize: 'small', marginTop: '20px' }}>
+              Note: If your launchers dont start, make sure force compatability is checked, shortcut properties are right and your steam files are updated. Remember to also edit your controller layout configurations if necessary! If all else fails, restart your steam deck manually.
+          </p>
+          <Focusable>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <DialogButton
+                          style={{ width: "fit-content" }}
+                          onClick={() => handleInstallClick("Install")}
+                          disabled={options.every(option => option.enabled === false)}
+                      >
+                          Install
+                      </DialogButton>
+                      <DialogButton
+                          style={{ width: "fit-content", marginLeft: "10px", marginRight: "10px" }}
+                          onClick={() => handleInstallClick("Uninstall")}
+                          disabled={options.every(option => option.enabled === false)}
+                      >
+                          Uninstall
+                      </DialogButton>
+                  </div>
+                  <ToggleField label="Separate Launcher Folders" checked={separateAppIds} onChange={handleSeparateAppIdsToggle} />
+              </div>
+          </Focusable>
+      </ModalRoot>
+  )
 };
