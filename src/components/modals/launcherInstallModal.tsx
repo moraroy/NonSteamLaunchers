752bc98ff73c205d048c4dfaca83ffb8bfd64504
Roy <88516395+moraroy@users.noreply.github.com>
2024-08-17 08:27:31 +0000

    Testing Back Button

--- src/components/modals/launcherInstallModal.tsx
+++ src/components/modals/launcherInstallModal.tsx
@@ -185,7 +185,7 @@ export const LauncherInstallModal: VFC<LauncherInstallModalProps> = ({ closeModa
             {currentLauncher && (
                 <img src={launcherImages[currentLauncher]} alt="Overlay" style={{ ...fadeStyle, opacity: 0.5 }} />
             )}
-            <DialogButton onClick={cancelOperation}>Cancel</DialogButton>
+            <DialogButton onClick={cancelOperation}>Back</DialogButton>
         </DialogBody>
     </ModalRoot> :
     <ModalRoot onCancel={closeModal}>
