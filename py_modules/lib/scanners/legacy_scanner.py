9f5d2791829fa0a52be3087414dfd3363acf56d7
Roy <88516395+moraroy@users.noreply.github.com>
2024-09-26 06:16:20 +0000

    fixed scanners

--- py_modules/lib/scanners/legacy_scanner.py
+++ py_modules/lib/scanners/legacy_scanner.py
@@ -7,18 +7,26 @@ import platform
 if platform.system() == "Windows":
     import winreg
 
+def convert_unix_to_windows_path(unix_path):
+    return unix_path.replace('/c/', 'C:\\').replace('/', '\\')
+
 def legacy_games_scanner(logged_in_home, legacy_launcher, create_new_entry):
     if platform.system() == "Windows":
-        legacy_dir = os.path.join(logged_in_home, "AppData", "Local", "Legacy Games")
-        user_reg_path = None  # Not needed for Windows
+        legacy_dir = "C:\\Program Files\\Legacy Games"
+        user_reg_path = None
     else:
         legacy_dir = f"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{legacy_launcher}/pfx/drive_c/Program Files/Legacy Games/"
         user_reg_path = f"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{legacy_launcher}/pfx/user.reg"
 
-    if not os.path.exists(legacy_dir) or (user_reg_path and not os.path.exists(user_reg_path)):
-        print("Path not found. Skipping Legacy Games Scanner.")
+    decky_plugin.logger.info(f"Platform: {platform.system()}")
+    decky_plugin.logger.info(f"Legacy Directory: {legacy_dir}")
+    decky_plugin.logger.info(f"User Reg Path: {user_reg_path}")
+
+    if not os.path.exists(legacy_dir) or (user_reg_path is not None and not os.path.exists(user_reg_path)):
+        decky_plugin.logger.info("Path not found. Skipping Legacy Games Scanner.")
     else:
-        print("Directory and user.reg file found.")
+        decky_plugin.logger.info("Directory and user.reg file found.")
+
         if platform.system() == "Windows":
             user_reg = get_windows_registry_entries()
         else:
@@ -29,7 +37,7 @@ def legacy_games_scanner(logged_in_home, legacy_launcher, create_new_entry):
             if game_dir == "Legacy Games Launcher":
                 continue
 
-            print(f"Processing game directory: {game_dir}")
+            decky_plugin.logger.info(f"Processing game directory: {game_dir}")
 
             if game_dir == "100 Doors Escape from School":
                 app_info_path = os.path.join(legacy_dir, "100 Doors Escape from School", "100 Doors Escape From School_Data", "app.info")
@@ -39,28 +47,29 @@ def legacy_games_scanner(logged_in_home, legacy_launcher, create_new_entry):
                 exe_path = os.path.join(legacy_dir, game_dir, game_dir.replace(" ", "") + ".exe")
 
             if os.path.exists(app_info_path):
-                print("app.info file found.")
+                decky_plugin.logger.info("app.info file found.")
                 with open(app_info_path, 'r') as file:
                     lines = file.read().split('\n')
                     game_name = lines[1].strip()
-                    print(f"Game Name: {game_name}")
+                    decky_plugin.logger.info(f"Game Name: {game_name}")
             else:
-                print("No app.info file found.")
+                decky_plugin.logger.info("No app.info file found.")
 
             if os.path.exists(exe_path):
                 game_exe_reg = re.search(r'\[Software\\\\Legacy Games\\\\' + re.escape(game_dir) + r'\].*?"GameExe"="([^"]*)"', user_reg, re.DOTALL | re.IGNORECASE)
                 if game_exe_reg and game_exe_reg.group(1).lower() == os.path.basename(exe_path).lower():
-                    print(f"GameExe found in user.reg: {game_exe_reg.group(1)}")
+                    decky_plugin.logger.info(f"GameExe found in user.reg: {game_exe_reg.group(1)}")
                     start_dir = os.path.join(legacy_dir, game_dir)
                     if platform.system() == "Windows":
-                        launch_options = f"\"{exe_path}\""
+                        launch_options = ""
+                        create_new_entry(exe_path, game_name, launch_options, start_dir, "Legacy Games")
                     else:
                         launch_options = f"STEAM_COMPAT_DATA_PATH=\"{logged_in_home}/.local/share/Steam/steamapps/compatdata/{legacy_launcher}\" %command%"
-                    create_new_entry(f'"{exe_path}"', game_name, launch_options, f'"{start_dir}"', "Legacy Games")
+                        create_new_entry(f'"{exe_path}"', game_name, launch_options, f'"{start_dir}"', "Legacy Games")
                 else:
-                    print(f"No matching .exe file found for game: {game_dir}")
+                    decky_plugin.logger.info(f"No matching .exe file found for game: {game_dir}")
             else:
-                print(f"No .exe file found for game: {game_dir}")
+                decky_plugin.logger.info(f"No .exe file found for game: {game_dir}")
 
 def get_windows_registry_entries():
     registry_entries = ""
