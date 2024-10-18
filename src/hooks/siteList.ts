7fe1c327592e62d00c89f35b13ca5a4d09524c5a
Roy <88516395+moraroy@users.noreply.github.com>
2024-10-18 10:19:18 +0000

    added site list for users to add their own sites

---
+++ src/hooks/siteList.ts
@@ -0,0 +1,211 @@
+export const sitesList = [
+    {
+      name: 'epicGames',
+      label: 'Epic Games',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/164fbf608021ece8933758ee2b28dd7d.jpg'
+    },
+    {
+      name: 'gogGalaxy',
+      label: 'Gog Galaxy',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/ce016f59ecc2366a43e1c96a4774d167.jpg'
+    },
+    {
+      name: 'uplay',
+      label: 'Ubisoft Connect',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/525d57e5f56f04be298e821454ced9bc.png'
+    },
+    {
+      name: 'battleNet',
+      label: 'Battle.net',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/9f319422ca17b1082ea49820353f14ab.jpg'
+    },
+    {
+      name: 'amazonGames',
+      label: 'Amazon Games',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/a70145bf8b173e4496b554ce57969e24.jpg'
+    },
+    {
+      name: 'eaApp',
+      label: 'EA App',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/6458ed5e1bb03b8da47c065c2f647b26.jpg'
+    },
+    {
+      name: 'legacyGames',
+      label: 'Legacy Games',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/86cfeb447e7f474a00adb7423c605875.jpg'
+    },
+    {
+      name: 'itchIo',
+      label: 'Itch.io',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://i.pcmag.com/imagery/reviews/044PXMK6FlED1dNwOXkecXV-4.fit_scale.size_1028x578.v1597354669.jpg'
+    },
+    {
+      name: 'humbleGames',
+      label: 'Humble Games',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/4cb3ded67cb7a539395ab873354a01c1.jpg'
+    },
+    {
+      name: 'indieGala',
+      label: 'IndieGala Client',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/8348173ba70a643e9d0077c1605ce0ad.jpg'
+    },
+    {
+      name: 'rockstarGamesLauncher',
+      label: 'Rockstar Games Launcher',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/60b4ddba6215df686ff6ab71d0c078e9.jpg'
+    },
+    {
+      name: 'psPlus',
+      label: 'Playstation Plus',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/6c037a13a7e2d089a0f88f86b6405daf.jpg'
+    },
+    {
+      name: 'hoyoPlay',
+      label: 'HoYoPlay',
+      URL: '',
+      streaming: false,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/3a4cffbfa1ae7220344b83ea754c46c4.jpg'
+    },
+    //these are streaming sites vvv
+    {
+      name: 'xboxGamePass',
+      label: 'Xbox Game Pass',
+      URL: 'https://www.xbox.com/play',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/167b7d08b38facb1c06185861a5845dd.jpg'
+    },
+    {
+      name: 'fortnite',
+      label: 'Fortnite (xCloud)',
+      URL: 'https://www.xbox.com/en-US/play/games/fortnite/BT5P2X999VH2/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/560cc70f255b94b8408709e810914593.jpg'
+    },
+    {
+      name: 'geforceNow',
+      label: 'GeForce Now',
+      URL: 'https://play.geforcenow.com',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/5e7e6e76699ea804c65b0c37974c660c.jpg'
+    },
+    {
+      name: 'amazonLuna',
+      label: 'Amazon Luna',
+      URL: 'https://luna.amazon.com/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/5966577c1d725b37c26c3f7aa493dd9c.jpg'
+    },
+    {
+      name: 'webRcade',
+      label: 'WebRcade',
+      URL: 'https://play.webrcade.com/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/f9b8cc42051c6d1c1ddaf5260118d585.jpg'
+    },
+    {
+      name: 'webRcadeeditor',
+      label: 'WebRcade (Editor)',
+      URL: 'https://editor.webrcade.com/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/f9b8cc42051c6d1c1ddaf5260118d585.jpg'
+    },
+    {
+      name: 'netflix',
+      label: 'Netflix',
+      URL: 'https://www.netflix.com',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/119f6887f5ebfd6d5b40213819263e68.jpg'
+    },
+    {
+      name: 'hulu',
+      label: 'Hulu',
+      URL: 'https://www.hulu.com/welcome',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/4bbddbaea593148384a27a8dcf498d30.jpg'
+    },
+    {
+      name: 'disneyPlus',
+      label: 'Disney+',
+      URL: 'https://www.disneyplus.com',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/0dad24dc5419076f64f2ba93833b354e.png'
+    },
+    {
+      name: 'amazonPrimeVideo',
+      label: 'Amazon Prime Video',
+      URL: 'https://www.amazon.com/primevideo',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/hero_thumb/5e7cefa9b606dcd7b0faa082d82cdb1d.jpg'
+    },
+    {
+      name: 'youtube',
+      label: 'Youtube',
+      URL: 'https://www.youtube.com',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/786929ce1b2e187510aca9b04a0f7254.jpg'
+    },
+    {
+      name: 'crunchyroll',
+      label: 'Crunchyroll',
+      URL: 'https://www.crunchyroll.com/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/b51869faee0c2357dc5c2c34e4229a80.jpg'
+    },
+    {
+      name: 'appletv+',
+      label: 'Apple TV+',
+      URL: 'https://tv.apple.com/',
+      streaming: true,
+      enabled: false,
+      urlimage: 'https://cdn2.steamgriddb.com/thumb/305aefb84d348c156953f7a3d4aa3e04.jpg'
+    }
+]
